cff4e4d739b52c851c244f811c40f8301067d3b0
futzone <futzoneuz@gmail.com>
2025-05-10 21:37:49 +0500

    30483849839483948

--- lib/src/core/constants/app_constants.dart
+++ lib/src/core/constants/app_constants.dart
@@ -1,3 +1,5 @@
+import 'package:url_launcher/url_launcher_string.dart';
+
 class AppConstants {
   static const String androidApp = "";
   static const String iOSApp = "";
@@ -5,7 +7,9 @@ class AppConstants {
   static const String telegramChannel = "";
   static const String website = "";
 
-  static Future<void> launchAppFor(platform) async {}
+  static Future<void> launchAppFor(platform) async {
+    await launchUrlString("https://www.imlogo.uz/");
+  }
 
   static const String aboutText =
       "Loyiha, Alisher Navoiy nomidagi Toshkent davlat o'zbek tili va adabiyoti universiteti, «Kompyuter lingvistikasi» yo'nalishi bitiruvchi kurs talabasi Abdumalikova Gulshoda tomonidan bitiruv malakaviy topshiriq ishi sifatida tayyorlandi.";
